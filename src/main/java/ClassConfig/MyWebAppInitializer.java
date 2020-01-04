2355e24d8ce9e4e25e68a7c36b2bd4200d2938b8
huangdeyi <huangdeyi@zsoft.com.cn>
2020-01-04 10:52:20 +0800

    web容器启动时的初始化原理

--- src/main/java/ClassConfig/MyWebAppInitializer.java
+++ src/main/java/ClassConfig/MyWebAppInitializer.java
@@ -1,13 +1,18 @@
 package ClassConfig;
 
-import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.Filter;
 
 //自定义加载DispatcherServlet和springContext的类
 //代替传统的web.xml文件，tomcat7.0版本之后可以在启动时扫描到这个类
 // 最后实现在容器启动时加载和初始化DispatcherServlet和spring容器配置类
+/**
+ * 原理：容器启动时会在类路径在中查找ServletContainerInitializer的实现类，该实现类要配合@HandlerTypes使用
+ *       SpringServletContainerInitializer实现了ServletContainerInitializer，并通过@HandlerTypes指定了配置
+ *      类的顶级接口WebApplicationInitializer。这样就会将类路径下WebApplicationInitializer及其实现类的Class
+ *      对象放到onStartUp(Set<Class<?>> webApplicationInitailizerClasses,ServletContext servletContext)的
+ *      Set集合参数中，然后通过反射创建WebApplicationInitializer实现类的实例，由Spring去实现剩下的初始化逻辑
+ *      使用的是AnnotationConfigWebApplicationContext容器加载器
+ * */
 public class MyWebAppInitializer  extends AbstractAnnotationConfigDispatcherServletInitializer {
     //返回spring容器的class对象
     protected Class<?>[] getRootConfigClasses() {
--- src/main/java/ClassConfig/SecondWebAppInitializer.java
+++ src/main/java/ClassConfig/SecondWebAppInitializer.java
@@ -7,7 +7,7 @@ import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 
-//web容器启动时可以加载多个初始化器
+//web容器启动时可以加载多个实现了WebApplicationInitializer的初始化器
 public class SecondWebAppInitializer implements WebApplicationInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
         //添加过滤器
